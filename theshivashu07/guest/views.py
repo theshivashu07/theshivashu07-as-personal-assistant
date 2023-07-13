8617f2bff75c2d006e2963044084105307246fca
Shivam Shukla <shivamshukla3108@gmail.com>
2023-07-13 19:16:46 +0530

    submission - after migrate, makemigrations, createsuperuser commands and basic updations on few files.

--- theshivashu07/guest/views.py
+++ theshivashu07/guest/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+
+def index(request):
+	return HttpResponse("Hey <b>Shivam Shukla</b>, <b style='font-size:10px'> And your profile-username is '@theshivashu07'</b> !!!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
